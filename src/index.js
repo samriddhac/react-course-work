d1cfe5a115b0a4797eda6c01caedbfe1006693d0
Samriddha Chatterjee <Samriddha.Chatterjee@Pearson.com>
2017-03-17 17:36:44 +0530

    React Chapter 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -28,9 +29,11 @@ class App extends Component {
 	}
 
 	render(){
+		const videoSearh = _.debounce(term => this.videoSearch(term), 300);
+
 		return (
 			<div>
-				<SearchBar onInputChange={term => this.videoSearch(term)}/>
+				<SearchBar onInputChange={videoSearh}/>
 				<VideoDetails video={this.state.selectedVideo} />
 				<VideoList 
 				onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
